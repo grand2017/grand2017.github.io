f892b17fd43b3f29894f8891bbe08604d41f711f
grand2017 <32708761+grand2017@users.noreply.github.com>
2017-10-12 10:08:29 +0300

    Member page

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,29 @@
 $('.slider').owlCarousel({
-  animateOut: 'fadeOutLeftBig',
-  animateIn: 'fadeInRightBig',
-	loop:true,
-	margin:10,
-	nav:true,
-	dots:false,
-	center: true,
-	autoplay:true,
-  autoplayTimeout:6000,
-	smartSpeed: 1000,
-	autoplayHoverPause:true,
-  items: 1
-});
+    animateOut: 'fadeOutLeftBig',
+    animateIn: 'fadeInRightBig',
+  	loop:true,
+  	margin:10,
+  	nav:true,
+  	dots:false,
+  	center: true,
+  	autoplay:true,
+    autoplayTimeout:6000,
+  	smartSpeed: 1000,
+  	autoplayHoverPause:true,
+    items: 1
+  });
 $('.about-slider').owlCarousel({
-	loop:true,
-	margin:10,
-	nav:false,
-	dots:true,
-	center: true,
-	autoplay:true,
-  autoplayTimeout:5000,
-	smartSpeed: 1000,
-	autoplayHoverPause:true,
-  items: 1
-});
+  	loop:true,
+  	margin:10,
+  	nav:false,
+  	dots:true,
+  	center: true,
+  	autoplay:true,
+    autoplayTimeout:5000,
+  	smartSpeed: 1000,
+  	autoplayHoverPause:true,
+    items: 1
+  });
 $('.special-carousel').owlCarousel({
 	loop:true,
 	margin:0,
@@ -108,17 +108,19 @@ $('.item__btn').click(function(){
   $('.special-popup').fadeIn();
 });
 function initMap() {
-	var myLatLng = {lat: 55.886767, lng: 37.436494};
-	var map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 15,
-		center: myLatLng,
-		scrollwheel: false,
-		disableDefaultUI: true
-	});
-	var marker = new google.maps.Marker({
-		position: myLatLng,
-		map: map,
-		title: 'МТК "ГРАНД"',
-		icon: 'img/icons/marker.svg'
-	});
+  if ($('#map').length) {
+    var myLatLng = {lat: 55.886767, lng: 37.436494};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: myLatLng,
+      scrollwheel: false,
+      disableDefaultUI: true
+    });
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'МТК "ГРАНД"',
+      icon: 'img/icons/marker.svg'
+    });
+  }
 }
